f367ec2824c03215f2d2e3c89ae725a91a90b513
NickyBrownlie <40165754@napier.ac.uk>
2017-11-14 17:10:21 +0000

    Data library made

---
+++ src/data.py
@@ -0,0 +1,97 @@
+LeafandBean = {
+  'Espresso': '1.80',    'EspressoLarge': '2.70',
+  'Americano': '2.10',   'AmericanoLarge': '3.00',
+  'Latte': '2.30',       'LatteLarge': '3.30',
+  'Cappuccino': '2.30',  'CappuccinoLarge': '3.30',
+  'Mocha': '2.75',       'MochaLarge': '3.85',
+  'FlatWhite': '2.30',   'FlatWhiteLarge': '3.30',
+  'HotChocolate': '2.70',
+}
+
+LaBarantine = {
+  'Espresso': '1.75',    'EspressoLarge': '1.95',
+  'Americano': '2.10',   'AmericanoLarge': '2.35',
+  'Latte': '2.40',       'LatteLarge': '2.50',
+  'Cappuccino': '2.40',  'CappuccinoLarge': '2.60',
+  'Mocha': '2.40',       'MochaLarge': '2.85',
+  'FlatWhite': '2.40',   'FlatWhiteLarge': '2.60',
+  'HotChocolate': '2.40','HotChocolateLarge': '2.75',
+}
+
+ThePod = {
+  'Espresso': '2.00',
+  'Americano': '2.45',
+  'Latte': '2.45',
+  'Cappuccino': '2.45',
+  'Mocha': '2.95',
+  'FlatWhite': '2.45',
+  'HotChocolate': '2.45'.
+}
+
+Starbucks = {
+  'Espresso': '1.60',    'EspressoMedium': '1.90',
+  'Americano': '2.00',   'AmericanoMedium': '2.29',   'AmericanoLarge': '2.50',
+  'Latte': '2.25',       'LatteMedium': '2.60',       'LatteLarge': '2.95',
+  'Cappuccino': '2.25',  'CappuccinoMedium': '2.60',  'CappuccinoLarge': '2.95',
+  'Mocha': '2.80',       'MochaMedium': '3.10',       'MochaLarge': '3.40',
+  'FlatWhite': '2.60',
+  'HotChocolate': '2.65','HotChocolateMedium': '2.95','HotChocolateLarge': '3.15',
+}
+
+Costa = {
+  'Espresso': '1.45',    'EspressoMedium': '1.80',
+  'Americano': '1.95',   'AmericanoMedium': '2.20',   'AmericanoLarge': '2.40',
+  'Latte': '2.15',       'LatteMedium': '2.45',       'LatteLarge': '2.65',
+  'Cappuccino': '2.25'   'CappuccinoMedium': '2.55',  'CappuccinoLarge': '2.65',
+  'Mocha': '2.50',       'MochaMedium': '2.80',       'MochaLarge': '2.90',
+  'FlatWhite': '2.40',
+  'HotChocolate': '2.40','HotChocolateMedium': '2.70','HotChocolateLarge': '2.90',
+}
+
+ProjectCoffee = {
+  'Espresso': '1.60',    'Espressolarge': '1.90',
+  'Americano': '1.95',   'AmericanoLarge': '2.50',
+  'Latte': '2.05',       'LatteLarge': '2.60',
+  'Cappuccino': '2.05',  'CappuccinoLarge': '2.60',
+  'Mocha': '2.70',
+  'HotChocolate': '2.60',
+}
+
+OrganicDeliciousCafe = {
+  'Espresso': '2.40',
+  'Americano': '2.70',
+  'Latte': '2.95',
+  'Cappuccino': '2.95',
+  'Mocha': '3.00',
+  'FlatWhite': '3.00',
+}
+
+CafeNero = {
+  'Espresso': '1.65',
+  'Americano': '1.85',   'AmericanoMedium': '2.15',   'AmericanoLarge': '2.45',
+  'Latte': '2.05',       'LatteMedium': '2.45',       'LatteLarge': '2.75',
+  'Cappuccino': '2.05',  'CappuccinoMedium': '2.75',  'CappuccinoLarge': '2.75',
+  'Mocha': '2.20',       'MochaMedium': '2.60',       'MochaLarge': '2.90',
+  'FlatWhite': '',
+  'HotChocolate': '2.20','HotChocolateMedium': '2.60','HotChocolateLarge': '2.90',
+}
+
+Greggs = {
+  'Espresso': '1.35',
+  'Americano': '2.50',   'AmericanoLarge': '1.75',
+  'Latte': '1.75',       'LatteLarge': '2.00',
+  'Cappuccino': '1.75',  'CappuccinoLarge': '2.00',
+  'Mocha': '1.75',       'MochaLarge': '2.00',
+  'FlatWhite': '1.50',   'FlatWhiteLarge': '1.75',
+  'HotChocolate': '1.65','HotChocolate': '1.90',
+}
+
+PieceBox = {
+  'Espresso': '2.00',
+  'Americano': '2.00',   'AmericanoLarge': '2.30',
+  'Latte': '2.30',       'LatteLarge': '2.60',
+  'Cappuccino': '2.30',  'CappuccinoLarge': '2.60',
+  'Mocha': '2.30',       'MochaLarge': '2.60',
+  'FlatWhite': '2.60',
+  'HotChocolate': '2.20','HotChocolateLarge': '2.50',
+}
