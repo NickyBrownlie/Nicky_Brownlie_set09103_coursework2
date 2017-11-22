3ef8a70f418990872f3414f6dcab6b8c077773de
NickyBrownlie <40165754@napier.ac.uk>
2017-11-22 15:21:09 +0000

    Added login feature, need to add append prices page

--- src/run.py
+++ src/run.py
@@ -1,7 +1,50 @@
-from flask import Flask, render_template
+import bcrypt
+from functools import wraps
+from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask
 app = Flask(__name__, template_folder='/home/tc/Nicky_Brownlie_set09103_coursework2/src/templates')
 
 from data import *
+
+app.secret_key = 'A0Zr98j /3 yX R~XHH!jmN]LWX /,? RT '
+
+valid_email = 'admin@admin'
+valid_pwhash = bcrypt.hashpw('admin', bcrypt.gensalt())
+
+def check_auth(email, password):
+  if(email == valid_email and valid_pwhash == bcrypt.hashpw(password.encode('utf-8'), valid_pwhash)):
+        return True
+  return False
+
+def requires_login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        status = session.get('logged_in', False)
+        if not status:
+          return redirect(url_for('.root'))
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/logout/')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('.root'))
+
+@app.route("/secret/")
+@requires_login
+def secret():
+    return "Secret Page"
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['email']
+        pw = request.form['password']
+
+        if check_auth(request.form['email'], request.form['password']):
+          session['logged_in'] = True
+          return redirect(url_for('.secret'))
+    return render_template('login.html')
 
 @app.route('/')
 def homepage():
